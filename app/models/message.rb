ccaf5563dfc8f12dbe875b271101ee18e726779e
Alve <114334244+AlveBrekXtreme@users.noreply.github.com>
2024-04-17 13:02:54 +0200

    Style the chatroom a bit

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,7 @@ class Message < ApplicationRecord
   after_create_commit {broadcast_replace_to :show_messages, target: self.channel.created_at , partial: 'messages/channel_messages', locals: { channel: self.channel } }
 
 
-  default_scope { order(created_at: :asc) }
+  default_scope { order(created_at: :desc) }
   belongs_to :channel
   belongs_to :user
 end
